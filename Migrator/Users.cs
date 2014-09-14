b0289d71101c141a3de2da0a24dc089e2b7c3b99
cyber48 <adrian92_pl@yahoo.com>
2014-09-14 21:22:02 +0300

    Start up configs

---
+++ Migrator/Users.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Migrator
+{
+    [Migration(201409141939)]
+    public class Users : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Create.Table("Users").WithColumn("Id").AsInt32().NotNullable()
+                                 .WithColumn("Name").AsString().NotNullable();
+                
+        }
+    }
+
+    [Migration(201409142115)]
+    public class AddColumnToUsers : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Users").AddColumn("Password").AsString().NotNullable();
+        }
+    }
+
+}
